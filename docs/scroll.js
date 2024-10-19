3059aefd790a5262ffe121e34152057e2b6b114b
Diana Petrovčíková <63262388+petrovcikovadiana@users.noreply.github.com>
2024-10-19 13:09:48 +0200

    update comments

--- docs/scroll.js
+++ docs/scroll.js
@@ -1,21 +1,21 @@
-// scrolling without navigation bar overlapping
+// Scrolling without navigation bar overlapping
 document.addEventListener("DOMContentLoaded", function () {
   const navHeight = document.querySelector("nav").offsetHeight;
-  const menuLinks = document.querySelectorAll('a[href^="#"]'); // Všechny odkazy na kotvy
+  const menuLinks = document.querySelectorAll('a[href^="#"]');
 
   menuLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
-      event.preventDefault(); // Zabrání základnímu chování
+      event.preventDefault();
 
-      const targetId = this.getAttribute("href").substring(1); // Získání ID cílové sekce
+      const targetId = this.getAttribute("href").substring(1);
       const targetSection = document.getElementById(targetId);
 
       if (targetSection) {
-        const targetPosition = targetSection.offsetTop - navHeight; // Odsazení s ohledem na výšku navigace
+        const targetPosition = targetSection.offsetTop - navHeight;
 
         window.scrollTo({
           top: targetPosition,
-          behavior: "smooth", // Plynulé scrollování
+          behavior: "smooth",
         });
       }
     });
